68df045cf67ec097d2ffcb2cc4cb2b6680176fe2
AleksandrVishniakov <vishniakov.alexandr@gmail.com>
2025-03-31 10:09:54 +0300

    Add initial files

---
+++ city_farm_class.py
@@ -0,0 +1,82 @@
+from json import load, dump
+from datetime import datetime, time, timedelta
+import RPi.GPIO as GPIO
+import os 
+
+
+#Настройка пинов
+
+GPIO.setmode(GPIO.BCM)
+ALL_PINS = [5, 6, 19, 20, 21, 26]
+for i in ALL_PINS:
+    GPIO.setup(i, 0, initial=GPIO.HIGH)
+GPIO.setup(23, 1, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(27, 1, pull_up_down=GPIO.PUD_UP)
+
+
+
+#Переменная для общения с диплеем
+end_byte = b'\xff\xff\xff'
+
+      
+def convert_time(item, hour=0):
+    h = int(item.split(':')[0])
+    m = int(item.split(':')[1])
+    return(datetime.combine(datetime.today(), time(hour=h, minute=m)) + timedelta(hours=int(hour)))
+
+def read_file(path_to_file='settings.json'):
+    if os.path.exists('settings.json'):
+        with open(path_to_file, 'r') as f:
+            data = load(f)
+        return data
+    else:
+        write_file({
+            "lamp_set": ["13:00","21:04"],
+            "water_day": 6,
+            "water_night": 2,
+            "time_water": 7
+        })
+
+def write_file(_list,path_to_file='settings.json'):
+    if os.path.exists('settings.json'):
+        with open(path_to_file, 'w') as f:
+            dump(_list, f, indent=4)
+    else:
+        write_file({
+            "lamp_set": ["13:00","21:04"],
+            "water_day": 6,
+            "water_night": 2,
+            "time_water": 7
+        })
+
+
+def watering(start, end, time_watering, watering_day, watering_night):
+    day = timedelta(hours=23, minutes=59) - timedelta(hours=start.hour, minutes=start.minute)
+    night = timedelta(hours=23, minutes=59) - day
+    time_watering = time_watering
+    interval_day = day / int(watering_day)
+    interval_night = night / int(watering_night)
+    watering_list = []
+    for i in range(int(watering_day)):
+        time_s = start + interval_day * i
+        time_e = time_s + timedelta(minutes=int(time_watering))
+        watering_list.append([time_s, time_e])
+    for i in range(int(watering_night)):
+        time_s = end + interval_night * i
+        time_e = time_s + timedelta(minutes=int(time_watering))
+        watering_list.append([time_s, time_e])
+    for i in watering_list:
+        if i[0] > datetime.now():
+            return i
+
+
+def convert_val(name, value):
+    name = str(name).encode()
+    value = str(value).encode()
+    return name + b'=' + value + end_byte
+
+
+def convert_txt(name, value):
+    name = str(name).encode()
+    value = str(value).encode()
+    return name + b'=' + b'"' + value + b'"' + end_byte
---
+++ start.sh
@@ -0,0 +1,3 @@
+sleep 10
+cd /home/agroaspect/Documents/city_farm_display_control/
+
