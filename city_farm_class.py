c4f2553ed4b36eb9fd547514d5b426a29258da28
AleksandrVishniakov <vishniakov.alexandr@gmail.com>
2025-04-14 15:37:32 +0300

    Fix watering

--- city_farm_class.py
+++ city_farm_class.py
@@ -3,17 +3,18 @@ from datetime import datetime, time, timedelta
 import RPi.GPIO as GPIO
 import os
 
-from devices import IDevice
+from devices.devices import IDevice
 
 # Переменная для общения с дисплеем
 end_byte = b'\xff\xff\xff'
+
 
 def setup_devices(devices: list[IDevice], debug: bool = True):
     if debug: print("[setup]")
 
     for device in devices:
         GPIO.setup(device.get_pin(), 0, initial=GPIO.HIGH)
-        if debug: print(">>", device.get_name(), "connected;")
+        if debug: print("  ", device.get_name(), "connected;")
 
     if debug: print()
 
@@ -23,7 +24,10 @@ def convert_time(item, hour=0):
     m = int(item.split(':')[1])
     return datetime.combine(datetime.today(), time(hour=h, minute=m)) + timedelta(hours=int(hour))
 
+
 last_settings = None
+
+
 def read_file(path_to_file='settings.json'):
     global last_settings
     if os.path.exists('settings.json'):
@@ -35,44 +39,44 @@ def read_file(path_to_file='settings.json'):
         return data
     else:
         write_file({
-            "lamp_set": ["13:00", "21:04"],
-            "water_day": 6,
-            "water_night": 2,
-            "time_water": 7
+            "lamp_set": [
+                "07:00",
+                "21:00"
+            ],
+            "water_day": 1,
+            "water_night": 0,
+            "time_water": 2
         })
 
 
 def write_file(_list, path_to_file='settings.json'):
-    if os.path.exists('settings.json'):
-        with open(path_to_file, 'w') as f:
+    with open(path_to_file, 'w') as f:
             dump(_list, f, indent=4)
-    else:
-        write_file({
-            "lamp_set": ["13:00", "21:04"],
-            "water_day": 6,
-            "water_night": 2,
-            "time_water": 7
-        })
 
 
-def watering(start, end, time_watering, watering_day, watering_night):
-    day = timedelta(hours=23, minutes=59) - timedelta(hours=start.hour, minutes=start.minute)
-    night = timedelta(hours=23, minutes=59) - day
-    time_watering = time_watering
-    interval_day = day / int(watering_day)
-    interval_night = night / int(watering_night)
-    watering_list = []
-    for i in range(int(watering_day)):
-        time_s = start + interval_day * i
-        time_e = time_s + timedelta(minutes=int(time_watering))
-        watering_list.append([time_s, time_e])
-    for i in range(int(watering_night)):
-        time_s = end + interval_night * i
-        time_e = time_s + timedelta(minutes=int(time_watering))
-        watering_list.append([time_s, time_e])
-    for i in watering_list:
-        if i[0] > datetime.now():
-            return i
+def watering(start: datetime, end: datetime, time_watering: int, watering_day: int, watering_night: int):
+    now = datetime.now()
+    daytime = end - start
+    night = timedelta(hours=24) - daytime
+    interval_day = daytime / watering_day if watering_day > 0 else timedelta()
+    interval_night = night / watering_night if watering_night > 0 else timedelta()
+    all_events = []
+    for days in [0, 1]:
+        for i in range(watering_day):
+            event_start = (start + timedelta(days=days)) + interval_day * (i+0.5)
+            event_end = event_start + timedelta(minutes=time_watering)
+            all_events.append((event_start, event_end))
+    for days in [0, 1]:
+        for i in range(watering_night):
+            event_start = (end + timedelta(days=days)) + interval_night * (i+0.5)
+            event_end = event_start + timedelta(minutes=time_watering)
+            all_events.append((event_start, event_end))
+    all_events.sort(key=lambda x: x[0])
+    for event in all_events:
+        if event[1] < now: continue
+        return event
+
+    return None
 
 
 def convert_val(name, value):
@@ -86,11 +90,11 @@ def convert_txt(name, value):
     value = str(value).encode()
     return name + b'=' + b'"' + value + b'"' + end_byte
 
+
 def print_settings(settings):
     print("Settings:")
-    print(">>", "lamp_set:", f"{settings['lamp_set'][0]} — {settings['lamp_set'][1]}")
-    print(">>", "water_day", settings['water_day'])
-    print(">>", "water_night", settings['water_night'])
-    print(">>", "time_water", settings['time_water'])
+    print("  ", "lamp_set:", f"{settings['lamp_set'][0]} — {settings['lamp_set'][1]}")
+    print("  ", "water_day", settings['water_day'])
+    print("  ", "water_night", settings['water_night'])
+    print("  ", "time_water", settings['time_water'])
     print()
-
